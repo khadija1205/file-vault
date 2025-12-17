01996bd357968c59940420fdd2d05eafcd4e2447
khadija1205 <khadijac101@gmail.com>
2025-12-17 16:39:27 +0530

    Chore

--- backend/src/middlewares/authorization.ts
+++ backend/src/middlewares/authorization.ts
@@ -2,42 +2,41 @@ import { Request, Response, NextFunction } from 'express';
 import { prisma } from '../config/prisma';
 import { AppError } from '../types';
 
-
-
-export const checkFileAccess = async (req:Request, res:Response, next:NextFunction) => {
+export const checkFileAccess = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const fileId = req.params.fileId;
         const userId = req.userId;
 
-
         const file = await prisma.file.findUnique({
             where: { id: fileId },
             include: { shares: true }
-        }); 
-
+        });
 
         if (!file) {
             throw new AppError(404, 'File not found');
         }
 
-
         if (file.ownerId === userId) {
             return next();
         }
 
+        type Share = {
+            sharedWithId: string;
+            expiryDate?: string | Date;
+        };
 
         // share access
-        const validShare = file.shares.find(s => {
+        const validShare = file.shares.find((s: Share) => {
             const isExpired = s.expiryDate && new Date(s.expiryDate) < new Date();
             return s.sharedWithId === userId && !isExpired;
-        })
-
+        });
 
         if (!validShare) {
             throw new AppError(403, 'Access denied');
-        }
 
+            next();
+        }
     } catch (error) {
         next(error);
     }
-}
+};
